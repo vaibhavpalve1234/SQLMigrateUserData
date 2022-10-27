1e9cc2025f75f8fb9f870476c6e23db00e3f4e32
vaibhav_palve <vaibhav.palve@valyu.ai>
2022-10-27 20:14:08 +0530

    Signed-off-by: vaibhav_palve <vaibhav.palve@valyu.ai>
    update code committed

--- migrate/ewa_company_rule.js
+++ migrate/ewa_company_rule.js
@@ -15,7 +15,7 @@
 //     OLD_CLIENTID)
 
 const { connection } = require("../connection/myslconnection");
-const { rdbInstance } = require("../firebase/uat-firebase-connection");
+const { rdbInstance } = require("../firebase/prod-firebase-connection");
 const { asyncForEach } = require("../util/asyncForEach");
 const {  getAllClientsConfigInformation, getClientInformation } = require("../util/fetchData");
 
@@ -42,20 +42,43 @@ const migrateClientConfigInfoDeatils = async() =>{
   try {
       let clientConfigInfo = await getAllClientsConfigInformation()
     await asyncForEach(Object.keys(clientConfigInfo), async(client) =>{
-      let {ContractId, blackoutEndDay, blackoutStartDay,cutOffDay, cutOffDayMonth, payDay, payDayMonth, payrollDaysCount, payrollStartDay, settlementDay, settlementDayMonth} = clientConfigInfo[client]
+      let {ContractId, blackoutEndDay, blackoutStartDay,cutOffDay, cutOffDayMonth, payDay, payDayMonth, payrollDaysCount, payrollStartDay, settlementDay, settlementDayMonth} = clientConfigInfo[client] || {}
       // console.log(ContractId, blackoutEndDay, blackoutStartDay,cutOffDay, cutOffDayMonth, payDay, payDayMonth, payrollDaysCount, payrollStartDay, settlementDay, settlementDayMonth);
       let configData = rdbInstance.ref('Client/' + client);
       configData = await configData.once('value');
       configData = configData.val() || {};
+      let { clientId } = configData || {}
       let product = configData.defaultProductName;
       let productInArthmate = configData[product] || {};
-      let {rules, payDate, maximum_loan_amount, cgst, cutoffDate, min_loan_amount, payrollEndDate, payrollStartDate, settlementDate, sgst } = productInArthmate
-      
+      let {rules, payDate, maximum_loan_amount, cgst, cutoffDate, min_loan_amount, payrollEndDate, payrollStartDate, settlementDate, sgst } = productInArthmate || {}
+      let {max} = rules || {}
+      let {eligibilty_percentage} = max || {}
+      if(clientId){
+        inserUserData(clientId, ContractId, eligibilty_percentage,  min_loan_amount, maximum_loan_amount, blackoutStartDay, blackoutEndDay, payDate )
+      } 
     })
     console.log("done");
   } catch (error) {
     console.log(error);
   }
+}
+
+const inserUserData = (clientId, ContractId, eligibilty_percentage, min_loan_amount, maximum_loan_amount, blackoutStartDay, blackoutEndDay, payDate) => {
+  let result = connection.query(`select * from ewa_company_rule where OLD_CLIENTID = ${clientId}`, (err, result) => {
+    if (result.length > 0 ) {
+      console.log('user already present in db');
+      return result
+    }
+    else {
+      console.log("insert");
+      connection.query(`INSERT INTO ewa_company_rule (COMPANY_ID,MODE_ID,LIMIT_PERCENTAGE,LIMIT_MIN,LIMIT_MAX,BLACKOUT_START_DATE,BLACKOUT_END_DATE,RECOVERY_DATE,STATUS,CREATED_BY,CREATED_TS,UPDATED_BY,UPDATED_TS,OLD_CLIENTID) values('${clientId}','${ContractId}','${eligibilty_percentage}','${min_loan_amount}','${maximum_loan_amount}','${blackoutStartDay}','${blackoutEndDay}','${payDate}','Active','user','${formatDate(new Date())}','user','${formatDate(new Date())}','${clientId}')`, (err, res) => {
+        if (err) throw err;
+        console.log('Last insert ID:', res.insertId);
+      }
+      );
+    }
+  })
+  return { result }
 }
 
 migrateClientConfigInfoDeatils()
