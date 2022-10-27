92d18e0b2b192462d2c81967331718585ec66515
vaibhav_palve <vaibhav.palve@valyu.ai>
2022-10-27 18:36:09 +0530

    Signed-off-by: vaibhav_palve <vaibhav.palve@valyu.ai>
    changes in my committed code

--- migrate/ewa_user_company.js
+++ migrate/ewa_user_company.js
@@ -3,15 +3,15 @@
 //     ARTHUM_USER_ID: user id,
 //     NBFC_USER_ID: uuid,
 //     COMPANY_ID:company id ,
-//     HR_CODE:employee id,
+//     HR_CODE:"none",
 //     EMPLOYMENT_TYPE: employee type,
 //     SALARY:salary,
 //     KYC_STATUS: is data submited,
 //     STATUS:active,
-//     CREATED_BY:today date,
-//     CREATED_TS:user,
-//     UPDATED_BY:created,
-//     UPDATED_TS:user,
+//     CREATED_BY:user,
+//     CREATED_TS:today new,
+//     UPDATED_BY:user,
+//     UPDATED_TS:today new,
 //     OLD_CLIENTID:company id,
 //     OLD_USERIDENTIFIER:-phone number)
 const { connection } = require("../connection/myslconnection");
@@ -53,16 +53,14 @@ const migrateUserCompanyDeatils = async() =>{
       if(!Loan){
         return;
       }
-      let { state } = userProfile || {}
-      let {} = personal || {}
+      let { state, user_id } = userProfile || {}
+      let {isDataSubmissionComplete } = personal || {}
       let client = Object.keys(workProfile)[0] || []
       console.log(phoneNumber);
-      let {arthmateDocumentId, arthmate_line_id, arthmateUuid, available_amount, creditLineId, creditLimit,doj,product_name, salary, isKYCReportPDFGenerated} = workProfile[client].userData
+      let {arthmateDocumentId, arthmate_line_id, arthmateUuid, available_amount, creditLineId, creditLimit,doj,product_name, salary, isKYCReportPDFGenerated, employeeId, employeeType} = workProfile[client].userData
       let {clientId, } = workProfile[client].companyDetails
-      // let clientInfo = await getClientInformation(client, phoneNumber)
-      // let { clientId } = clientInfo
       if (workProfile) {
-        
+        inserUserData(phoneNumber, user_id, arthmateUuid, clientId, employeeId, employeeType, salary, isKYCReportPDFGenerated)
       }
     })
     console.log("done");
@@ -70,3 +68,22 @@ const migrateUserCompanyDeatils = async() =>{
     console.log(error);
   }
 }
+
+const inserUserData = (phoneNumber, user_id, arthmateUuid, clientId, employeeId, employeeType, salary, isKYCReportPDFGenerated) => {
+  let result = connection.query(`select * from ewa_user_company where OLD_USERIDENTIFIER = ${phoneNumber}`, (err, result) => {
+    if (result.length > 0) {
+      console.log('user already present in db');
+      return result
+    }
+    else {
+      console.log("insert");
+      connection.query(`INSERT INTO ewa_user_company (USER_ID,ARTHUM_USER_ID,NBFC_USER_ID,COMPANY_ID,HR_CODE,EMPLOYMENT_TYPE,SALARY,KYC_STATUS,STATUS,CREATED_BY,CREATED_TS,UPDATED_BY,UPDATED_TS,OLD_CLIENTID,OLD_USERIDENTIFIER) values('${user_id}','','${arthmateUuid}','${clientId}','','${employeeType}','${salary}','${isKYCReportPDFGenerated}','Active','user','${formatDate(new Date())}','user','${formatDate(new Date())}','${clientId}','${phoneNumber}')`, (err, res) => {
+        if (err) throw err;
+        console.log('Last insert ID:', res.insertId);
+      }
+      );
+    }
+  })
+  return { result }
+}
+migrateUserCompanyDeatils()
