1280e7e7b176e8e58116fb09ce26b6bd5bf61c06
vaibhav_palve <vaibhav.palve@valyu.ai>
2022-10-28 14:07:03 +0530

    commit code

--- query.sql
+++ query.sql
@@ -1,8 +1,8 @@
 use local_db;
 select * from local_db.ewa_company_master;
-select * from ewa_company_rule;
-select * from ewa_loan_details;
-select * from ewa_user;
-select * from ewa_user_bank_account;
-select * from ewa_user_company;
-select * from ewa_user_line;
+select * from local_db.ewa_company_rule;
+select * from local_db.ewa_loan_details;
+select * from local_db.ewa_user;
+select * from local_db.ewa_user_bank_account;
+select * from local_db.ewa_user_company;
+select * from local_db.ewa_user_line;
