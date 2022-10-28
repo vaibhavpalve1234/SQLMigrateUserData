0a01f416db36cfc6c7ae146c92ac0ae1fc1d37db
vaibhav_palve <vaibhav.palve@valyu.ai>
2022-10-28 14:04:37 +0530

    Signed-off-by: vaibhav_palve <vaibhav.palve@valyu.ai>
    changes in my committed code

--- query.sql
+++ query.sql
@@ -1,4 +1,8 @@
-SELECT * FROM local_db.ewa_user;
-SELECT *
-FROM local_db.ewa_user_bank_account
-WHERE USER_COMPANY_ID = 7001;
+use local_db;
+select * from local_db.ewa_company_master;
+select * from ewa_company_rule;
+select * from ewa_loan_details;
+select * from ewa_user;
+select * from ewa_user_bank_account;
+select * from ewa_user_company;
+select * from ewa_user_line;
